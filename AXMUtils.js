29ef393fe3f8e28e78596bda12b736255760cb28
Paul Vauterin <paul.vauterin@gmail.com>
2015-06-15 10:55:34 +0200

    Parse url query parameters

--- AXMUtils.js
+++ AXMUtils.js
@@ -54,6 +54,20 @@ define([
             return (typeof v == 'function');
         };
 
+        Module.getUrlParameters = function() {
+            var urlParams;
+            var match,
+                pl     = /\+/g,  // Regex for replacing addition symbol with a space
+                search = /([^&=]+)=?([^&]*)/g,
+                decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+                query  = window.location.search.substring(1);
+
+            urlParams = {};
+            while (match = search.exec(query))
+                urlParams[decode(match[1])] = decode(match[2]);
+            return urlParams;
+        };
+
 
         Module.valueRange = function(minValue, maxValue) {
             var range = Module.object('@ValueRange');
