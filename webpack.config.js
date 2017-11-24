d76538df61df250866c27fe7b0cb3ff47876bfd9
Michiel Vermeir <michiel.vermeir@agilent.com>
2017-11-24 14:35:41 +0100

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,41 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  entry: path.join(__dirname, 'src', 'AXM'),
+  entry: path.join(__dirname, "src", "AXM"),
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "axiom.js",
+    library: "axiom",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
-    rules: [{
-      test: /.js?$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules'),
-        path.resolve(__dirname, 'bower_components')
-      ],
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015']
+    rules: [
+      {
+        test: /.js?$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: [
+          path.resolve(__dirname, "node_modules"),
+          path.resolve(__dirname, "bower_components")
+        ],
+        loader: "babel-loader",
+        query: {
+          presets: ["env"]
+        }
       }
-    }]
+    ]
   },
   resolve: {
-    extensions: ['.json', '.js', '.css'],
+    extensions: [".json", ".js", ".css"],
+    modules: ["src"],
     alias: {
-      
+      "jquery": "lib/jquery",
+      "jquery_cookie": "lib/jquery_cookie",
+      "_": "lib/lodash",
+      "blob": "lib/Blob",
+      "filesaver": "lib/FileSaver",
+      "resumable": "externals/resumable",
+      "datetimepicker": "lib/datetimepicker/datetimepicker",
+      "codemirror": "lib/CodeMirror/lib/codemirror",
+      "codemirror_yaml": "lib/CodeMirror/modes/yaml/yaml"
     }
   },
-  devtool: 'source-map',
-  devServer: {
-    publicPath: path.join('/dist/')
-  }
+  devtool: "source-map"
 };
