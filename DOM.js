5ce1703f58988c6abff2bceab7deb97e85dfa0f7
Michiel <6299345+michielvermeir@users.noreply.github.com>
2020-03-25 12:53:30 +0000

    Update DOM.js
    Co-Authored-By: MichaelVyverman <MichaelVyverman@users.noreply.github.com>

--- DOM.js
+++ DOM.js
@@ -13,7 +13,7 @@
 //PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 //DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 //ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-define(["require", "jquery", "_", "AXM/AXMUtils", "AXM/Events"], function(require, $, _, AXMUtils) {
+define(["require", "jquery", "_", "AXM/AXMUtils", "AXM/Events"], function(require, $, _, AXMUtils, AXMEvents) {
     const delegate = require("delegate");
     const { kebabCase: paramCase }  = require("lodash");
 
